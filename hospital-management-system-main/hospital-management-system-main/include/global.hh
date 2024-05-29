5db7090687a75bdc1394da4f4362414e755d922f
Tito Jaya <mhmmdtjaya@gmail.com>
2024-05-29 23:17:28 +0700

    recovery

--- hospital-management-system-main/hospital-management-system-main/include/global.hh
+++
@@ -1,9 +0,0 @@
-#ifndef GLOBAL_HMS
-#define GLOBAL_HMS
-using namespace std;
-#include <string>
-
-extern int yyyymmdd;
-int power(int n = 1, int exp = 0);
-int strToNum(string s);
-#endif // !GLOBAL_HMS
